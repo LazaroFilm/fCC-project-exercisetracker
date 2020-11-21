17d8b229a0df927debe900e0a20c8a4dc44669f6
LazaroFilm <lazarofilm@gmail.com>
2020-11-21 22:17:49 +0000

    I don't fucking know anymore

--- server.js
+++ server.js
@@ -3,14 +3,19 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const { nanoid } = require('nanoid');
 const { Schema } = mongoose;
-mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/exercise-track', { useNewUrlParser: true, useUnifiedTopology: true } );
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/exercise-track', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+  });
 
 app.use(cors());
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-const urlencodedParser = bodyParser.urlencoded({extended: false});
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 // console.log(urlencodedParser);
 
 
@@ -20,38 +25,112 @@ app.get('/', (req, res) => {
 });
 
 // My Code //////////
-
 console.log("Hello World!");
 
 // Mongoose
 const usernameSchema = new Schema({
-  username: { type: String, required: true }
-});
-const User = mongoose.model("Profiles", usernameSchema)
+  _id: { type: String, required: true },
+  username: { type: String, required: true },
+  // description: { type: String, required: false },
+  // duration: { type: Number, required: false },
+  // date: { type: String, required: false }
+}, {versionKey: false});
+const User = mongoose.model("Profiles", usernameSchema);
 
+const exerciseSchema = new Schema({
+  userId: { type: String, required: true },
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
+  date: { type: String, required: true }
+}, {versionKey: false});
+const Exercise = mongoose.model("Sessions", exerciseSchema);
+
+// 
+const makeNewId = async () => {
+  let newId = nanoid(10);
+  // let newId = "aabbcc" // used to test already existing Id.
+  const idExists = await User.findOne({ "_id": newId });
+  if (idExists) {
+    console.log("ID already exists, generating a new one");
+    makeNewId();
+  } else {
+    return newId;
+  };
+};
+
+const dateRegEx = /^\d{4}-((0[1-9])|(1[0-2]))-(0[1-9]|([1-2]\d)|(3[0-1]))$/
 
 // POST new username and save it to the db. 
 app.post('/api/exercise/new-user', urlencodedParser, async (req, res) => {
   console.log(req.body);
-  const username = new User({
-    username: req.body.username
-  });
-  username.save((err, res) => {
-    if (err) console.log(err)
-  })
+  const usernameExists = await User.findOne({ "username": req.body.username });
+  if (req.body.username === "") {
+    res.send("Please ender a username")
+  } else if (usernameExists) {
+    res.send("Username already taken")
+  } else {
+    const newId = await makeNewId();
+    const username = new User({
+      _id: newId,
+      username: req.body.username
+    });
+    username.save((err, res) => {
+      if (err) console.log(err)
+    });
+    res.json({
+      _id: newId,
+      username: req.body.username
+    })
+  };
 });
 
+// GET all users
+app.get('/api/exercise/users', (req, res) => {
+  console.log("getting the colection")
+  // const collection = User.find({});
+  User.find({}, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(result);
+    };
+  });
+});
 
-
-// POST exercises
+// POST new exercises
 app.post('/api/exercise/add', urlencodedParser, async (req, res) => {
-  console.log("TODO new exercise");
   console.log(req.body);
+  const id = { "_id": req.body.userId }
+  const checkId = await User.findOne(id)
+  let sessionDate;
+  if (dateRegEx.test(req.body.date)) {
+    sessionDate = new Date(req.body.date);
+    // console.log("You entered the date: ", sessionDate.toISOString().split('T')[0])
+  } else {
+    sessionDate = new Date();
+    console.log("today's date is ", sessionDate)
+  }
+  console.log(sessionDate);
+  if (checkId && req.body.description && req.body.duration) {
+    const entry = {
+      description: req.body.description,
+      duration: req.body.duration,
+      date: sessionDate.toString().split(' 00:00')[0]
+    };
+    console.log("entry:", entry)
+    
+    // await User.findOneAndUpdate(id, entry)
+    // res.json(await User.findOne(id))
+    // console.log(await User.findOne(id))
+    console.log("#################")
+  } else {
+    res.send("Please fill all mandatory fields correctly")
+  };
 });
 
 // Not found middleware
 app.use((req, res, next) => {
-  return next({status: 404, message: 'not found'});
+  return next({ status: 404, message: 'not found' });
 });
 
 // Error Handling middleware
